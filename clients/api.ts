6c594d1febd5132b48d413b0d99d02beca9baa5c
Anas Khan <anascoder4@gmail.com>
2024-10-22 23:08:34 +0530

    changes

--- clients/api.ts
+++ clients/api.ts
@@ -4,7 +4,7 @@ import { GraphQLClient } from "graphql-request";
 const isClient = typeof window !== "undefined";
 
 // Create the GraphQL client with proper headers
-export const graphqlclient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL ||"http://twiiter-1590515209.ap-south-1.elb.amazonaws.com/graphql" , {
+export const graphqlclient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL ||"https://ddraugpcf035b.cloudfront.net/graphql" , {
   headers: () => {
     // Only attempt to get the token in the client-side environment
     const token = isClient ? window.localStorage.getItem("__twittertoken") : null;
